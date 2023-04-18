8879fbfc684deef5cddb72b4071e184bd747f0e2
yuristaindani <indanisukma@gmail.com>
2023-04-18 10:48:21 +0800

    Menambahkan public di depan static void main(String[] args)...

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    static HashMap<String, Restaurant> dataRestaurant = new HashMap<>();
+    public static HashMap<String, Restaurant> dataRestaurant = new HashMap<>();
 
     public static void main(String[] args) {
 
